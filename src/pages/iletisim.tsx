1e562cfd29b04cca2b79ac4aaecd5f9b3c573e29
Maeglin <lordtrai@yahoo.com>
2023-06-24 16:16:48 +0300

    Address change

--- src/pages/iletisim.tsx
+++ src/pages/iletisim.tsx
@@ -39,7 +39,7 @@ const Contact: React.FunctionComponent = () => (
             <div className="post-content" style={{ textAlign: 'center' }}>
               <iframe
                 style={{ border: '0.5px groove grey', padding: '0.1em' }}
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d633.0311410991918!2d28.931704293439847!3d40.21665046369536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd93b77779235c7ea!2s%C3%96zel+Rozet+Aile+Dan%C4%B1%C5%9Fma+Merkezi!5e1!3m2!1sen!2str!4v1564662761367!5m2!1sen!2str"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3046.6874019015136!2d28.893042599999998!3d40.216015999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ca11b494a5feeb%3A0xb8ee016bc75c6932!2sArkat%20Plaza%20Bursa!5e0!3m2!1sen!2str!4v1687090736710!5m2!1sen!2str"
                 title="Bize ulaşın"
                 width="100%"
                 height="450"
@@ -48,27 +48,25 @@ const Contact: React.FunctionComponent = () => (
               />
               <br />
               <Grid
-                width={320} gap={24} style={{ marginTop: '1em'}}>
-                <div  style={{ textAlign: 'left' }}>
+                width={320} gap={24} style={{ marginTop: '1em' }}>
+                <div style={{ textAlign: 'left' }}>
                   <strong>Açık Adresimiz: </strong>
                   <p>
-                    <address>🏢 Altınşehir Mahallesi 210. Sokak No:8 Nilüfer, Bursa</address>
+                    <address>🏢 Yüzüncüyıl, Arkat Plaza Bursa, İzmir Yolu Cd 12. Blok A, No: 3, 16120 Nilüfer/Bursa</address>
                   </p>
                   <strong>Email:</strong><p> <span>📧 info</span>
-                  <span>@</span>
-                  <span>rozetdanismanlik.com</span></p>
+                    <span>@</span>
+                    <span>rozetdanismanlik.com</span></p>
                 </div>
                 <div style={{ textAlign: 'left' }}>
                   <strong>Çalışma Saatleri: </strong>
-                  <p>Haftaiçi: 🕘 09:00 - 🕕 18:00 
-                  <br />
-                  Haftasonu: 🕘 09:00 - 🕓 16:00 
-                  <br /></p>
+                  <p>Haftaiçi: 🕘 09:00 - 🕕 18:00
+                    <br />
+                    Haftasonu: 🕘 09:00 - 🕓 16:00
+                    <br /></p>
                   <strong>Telefon Numaramız: </strong><p><a href="tel:+905432201816">📞 0543 220 18 16</a></p>
-
                 </div>
-              
-              </Grid>               
+              </Grid>
             </div>
           </PostFullContent>
         </article>
